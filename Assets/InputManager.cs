a4e63519885199d9a14524b8795b12cd933d3bba
ByzanTine <morbyzantine@gmail.com>
2015-01-20 02:31:18 -0500

    Simon Skeleton with even driven input handler

---
+++ Assets/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	private static InputManager instance;
+	private InputManager() {}
+	public static InputManager Instance
+	{
+		get
+		{
+			if (instance == null)
+				instance = GameObject.FindObjectOfType(typeof(InputManager)) as  InputManager;
+			return instance;
+		}
+	}
+	public LayerMask collideLayer;// Only collide with the colliders in this layer
+	// Event Handler
+	public delegate void OnKeyPress();
+
+	// Map to classical control
+	public event OnKeyPress OnKeyPress_Right;
+	public event OnKeyPress OnKeyPress_Left;
+	public event OnKeyPress OnKeyDown_Down;
+	public event OnKeyPress OnKeyUp_Down;
+	public event OnKeyPress OnKeyPress_Up;
+	public event OnKeyPress OnKeyDown_B;
+	public event OnKeyPress OnKeyDown_A;
+	// Handle our Ray and Hit
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.Comma)) {
+			OnKeyDown_A();
+		}
+		if (Input.GetKeyDown (KeyCode.X) || Input.GetKeyDown (KeyCode.Period)) {
+			OnKeyDown_B();
+		}
+		if (Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+			OnKeyDown_Down();
+		}
+		if (Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.DownArrow)) {
+			OnKeyUp_Down();
+		}
+
+	}
+}
