be78974fef6cac1dd2b917986cdaeb2187e58c3d
robinbach <bestname@umich.edu>
2015-01-23 17:56:22 -0500

    add torch set script and prefab

---
+++ Assets/_Scripts/OnWhipHitDestroy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnWhipHitDestroy : OnWhipEvent {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public override void onWhipEnter (){
+		StartCoroutine (revealItemAndDestroy());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator revealItemAndDestroy()
+	{
+		yield return new WaitForSeconds(0.1f);
+		Destroy (this.gameObject);
+	}
+}
+
+
