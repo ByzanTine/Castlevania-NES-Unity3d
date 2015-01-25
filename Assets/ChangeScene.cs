b0b4a4dc551b3cf988ab620495d98468598af344
robinbach <bestname@umich.edu>
2015-01-25 01:18:53 -0500

    update scene setup, adjust stair stage collider, add scene portal

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string targetScene;
+
+	void OnTriggerEnter2D( Collider2D coll ) {
+		Debug.Log ("rdy cg sce" + coll.gameObject.tag);
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log ("cg sce");
+			Application.LoadLevel (targetScene);
+		}
+	}
+}
