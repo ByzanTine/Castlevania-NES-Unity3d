e9f49ce6e68e2e18d469e6645108e655efb73808
robinbach <bestname@umich.edu>
2015-01-27 16:45:24 -0500

    update trap/ enemy trigger settings

---
+++ Assets/CreateMoster.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMoster : MonoBehaviour {
+
+	// all public var need to be init in Inspector
+	public GameObject  demonPrefab;
+	public int numberOfMonsters;
+	public Vector2 createAt;
+	private int demonNum = 0;
+	public float creationDelayInSec = 0.5f;
+	private float refreshTime = 5.0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			StartCoroutine(createMoster());
+		}
+	}
+
+	IEnumerator createMoster()
+	{
+		while (collider2D.enabled == true) {
+			for (; demonNum < numberOfMonsters; ++demonNum) {
+				GameObject gameObj = Instantiate (demonPrefab) as GameObject;
+				gameObj.transform.position = 
+					new Vector3 (transform.position.x + createAt.x, 
+            	transform.position.y + createAt.y, 0);
+				yield return new WaitForSeconds (creationDelayInSec);
+			}
+			yield return new WaitForSeconds (refreshTime);
+			demonNum = 0;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Disabled collider");
+			collider2D.enabled = false;
+		}
+	}
+}
--- Assets/_Scripts/Globals.cs
+++ Assets/_Scripts/Globals.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class Globals : MonoBehaviour {
 
 	public static float WhipLengthLong = 0.48f; // 48 pixel
-	public static float WhipLengthShort = 0.22f;  // 22 pixel
-	public static float PivotToWhipStart = 0.14f; // 14 pixel
+	public static float WhipLengthShort = 0.32f;  // fixed
+	public static float PivotToWhipStart = 0.00f; // fixed
 	public static float SquatOffset = -0.05f; // 5 pixel 
 	public static float WhipHeightOffset = 0.05f;// 5 pixel
 	public static float StairStepLength = 0.08f; // 8 pixel
