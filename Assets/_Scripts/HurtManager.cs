de2b8769ddd4ea62fa87e72e8c85ee8dd8e76bb2
ByzanTine <morbyzantine@gmail.com>
2015-02-01 03:49:19 -0500

    Fix the movement on the fly

--- Assets/_Scripts/HurtManager.cs
+++ Assets/_Scripts/HurtManager.cs
@@ -9,9 +9,11 @@ public class HurtManager : MonoBehaviour {
 	private bool hurting; // only provide accessor
 	public bool Hurting {
 		get {
-			return hurting;
+			return hurting || disableControl;
 		}
 	}
+
+	private bool disableControl;
 
 	private Animator animator;
 	private PlayerController pc;
@@ -25,33 +27,42 @@ public class HurtManager : MonoBehaviour {
 		sprite = GetComponent<SpriteRenderer> ();
 		status = GetComponent<StatusManager> ();
 		hurting = false;
+		disableControl = false;
+	}
+	// HACK
+	public bool onFlyHurting () {
+		return disableControl;
 	}
 	// ============================================================================ //
 	public IEnumerator Hurt () {
 
 		status.playerHealth -= 2;
-
+		Debug.Log("HURTING: Hurt fucntion called");
 		hurting = true;
 
 		// TODO do what ever is needed to turn off some collision
 		// CODE HERE
-
+		disableControl = true;
 
 		animator.SetBool ("Hurt", true);
+		pc.VerticalSpeed = 0; //reset to avoid fly high
 		pc.VerticalSpeed += initHurtVericalSpeed;
 		// add horizontal speed according to facing
 		pc.CurHorizontalVelocity = pc.facingRight? -1 : 1;
 		
 		yield return new WaitForSeconds (0.33f);
 		animator.SetBool ("Hurt", false);
+		Debug.Log("HURTING: fly state cleaned");
+
 		StartCoroutine (turnInvisible ());	
 		// turn 
+		animator.SetInteger ("Speed", 0);
 		animator.SetBool ("Squat", true);
 		
 		yield return new WaitForSeconds (0.33f);
 		pc.CurHorizontalVelocity = 0;
 		animator.SetBool ("Squat", false);
-
+		disableControl = false;
 		
 	}
 	
